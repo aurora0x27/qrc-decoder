e3f80ac7114291fd3492a94200c82ae94f65ea65
aurora0x27 <qingfengzl0831@163.com>
2025-01-12 00:11:51 +0800

    feat: refector qrc decoder and buffer

--- src/utils/include/utils/tripledes.h
+++ src/utils/include/utils/tripledes.h
@@ -1,29 +1,22 @@
 #ifndef TRIPLEDES_H
 #define TRIPLEDES_H
-#include <vector>
-#include <stdint.h>
-
+#include <cstdint>
 #include <utils/buffer.h>
+#include <vector>
 
-namespace qqmusic::utils
-{
-    enum class tripledes_crypt_mode
-    {
-        encrypt,
-        decrypt
-    };
+namespace qqmusic::utils {
+enum class tripledes_crypt_mode { encrypt, decrypt };
 
-    typedef std::vector<std::vector<std::vector<uint32_t>>> tripledes_key_schedule;
-    
-    // decrypt buf_in and append the result to the end of buf_out
-    void tripledes_crypt(qqmusic::utils::buffer*                buf_in,
-                         qqmusic::utils::buffer*                buf_out,
-                         qqmusic::utils::tripledes_key_schedule key_schedule);
+using tripledes_key_schedule = std::vector<std::vector<std::vector<uint32_t>>>;
 
-    qqmusic::utils::tripledes_key_schedule tripledes_key_setup(uint8_t*                             key,
-                                                               size_t                               key_size,
-                                                               qqmusic::utils::tripledes_crypt_mode mode);
+// decrypt buf_in and append the result to the end of buf_out
+void tripledes_crypt(qqmusic::utils::buffer& buf_in,
+                     qqmusic::utils::buffer& buf_out,
+                     qqmusic::utils::tripledes_key_schedule key_schedule);
 
-}
+qqmusic::utils::tripledes_key_schedule tripledes_key_setup(
+    const uint8_t* key, size_t key_size, qqmusic::utils::tripledes_crypt_mode mode);
+
+} // namespace qqmusic::utils
 
 #endif
--- src/utils/src/qrcdecoder.cc
+++ src/utils/src/qrcdecoder.cc
@@ -1,61 +1,46 @@
 #include <cstdint>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <utils/buffer.h>
+#include <utils/qrcdecoder.h>
+#include <utils/tripledes.h>
 #include <zlib.h>
 
-#include <utils/tripledes.h>
-#include <utils/qrcdecoder.h>
-#include <utils/buffer.h>
-
-static int decompress(qqmusic::utils::buffer* src,
-                      qqmusic::utils::buffer* dest);
+static int decompress(const qqmusic::utils::buffer& src, qqmusic::utils::buffer& dest);
 
 // qmc decoder
-static void qmc1_decrypt(qqmusic::utils::buffer* src);
+static void qmc1_decrypt(qqmusic::utils::buffer& src);
 
-qqmusic::result 
-qqmusic::utils::qrc_decode(qqmusic::utils::buffer*  src,
-                           qqmusic::utils::buffer*  dest,
-                           qqmusic::utils::qrc_type type)
-{
-    // copy a instance of src
-    qqmusic::utils::buffer* tmp = NULL;
+qqmusic::result qqmusic::utils::qrc_decode(const qqmusic::utils::buffer& src,
+                                           qqmusic::utils::buffer& dest,
+                                           qqmusic::utils::qrc_type type) {
+    qqmusic::utils::buffer tmp;
     if (type == qqmusic::utils::qrc_type::local) {
-        // local qrc needs qmc decrypt and delete 11 bytes in the beginning
-        qqmusic::utils::buffer pre_processed_buf(src->get_head(), src->get_size());
-        qmc1_decrypt(&pre_processed_buf);
-        try {
-            tmp = new qqmusic::utils::buffer(pre_processed_buf.get_head() + 11, pre_processed_buf.get_size() - 11);
-        } catch (std::bad_alloc& e) {
-            return qqmusic::result::mem_alloc_error;
-        }
-    } else if (type == qqmusic::utils::qrc_type::cloud) {
-        try {
-            tmp = new qqmusic::utils::buffer(src->get_head(), src->get_size());
-        } catch (std::bad_alloc& e) {
-            return qqmusic::result::mem_alloc_error;
-        }
+        qqmusic::utils::buffer raw(src);
+        qmc1_decrypt(raw);
+        tmp.assign(raw.begin() + 11, raw.end());
+    } else {
+        tmp = src;
     }
 
-    size_t tmp_size = tmp->get_size();
+    size_t tmp_size = tmp.size();
     // check if size is integer multiple of 8 bytes
     if (tmp_size % 8 != 0) {
-        return qqmusic::result::data_destroy;
+        return qqmusic::result({"data destroy"});
     }
 
     // QRC_KEY = b"!@#)(*$%123ZXC!@!@#)(NHL"
-    const     uint8_t qrc_key[] =
-    "\x21\x40\x23\x29\x28\x2A\x24\x25\x31\x32\x33\x5A\x58\x43\x21\x40\x21\x40\x23\x29\x28\x4E\x48\x4C";
+    constexpr uint8_t qrc_key[]
+        = "\x21\x40\x23\x29\x28\x2A\x24\x25\x31\x32\x33\x5A\x58\x43\x21\x40\x21\x40\x23\x29\x28\x4E\x48\x4C";
 
-    constexpr size_t  qrc_key_size = sizeof(qrc_key) - 1;
+    constexpr size_t qrc_key_size = sizeof(qrc_key) - 1;
 
     // generate key schedule
-    qqmusic::utils::tripledes_key_schedule schedule = qqmusic::utils::tripledes_key_setup((uint8_t*)qrc_key,
-                                                                                           qrc_key_size,
-                                                                                           qqmusic::utils::tripledes_crypt_mode::decrypt);
+    const auto schedule
+        = qqmusic::utils::tripledes_key_setup(qrc_key,
+                                              qrc_key_size,
+                                              qqmusic::utils::tripledes_crypt_mode::decrypt);
 
     qqmusic::utils::buffer compressed_buffer;
 
@@ -63,42 +48,38 @@ qqmusic::utils::qrc_decode(qqmusic::utils::buffer*  src,
     // origin python code:
     // for i in range(0, len(encrypted_text_byte), 8):
     //     data += tripledes_crypt(encrypted_text_byte[i:], schedule)
-    uint8_t* head = tmp->get_head();
+    uint8_t* head = tmp.data();
     for (size_t i = 0; i < tmp_size / 8; ++i) {
         qqmusic::utils::buffer tmp_section(head + i * 8, 8);
-        qqmusic::utils::tripledes_crypt(&tmp_section, &compressed_buffer, schedule);
+        qqmusic::utils::tripledes_crypt(tmp_section, compressed_buffer, schedule);
     }
-    delete tmp;
 
     // decompress the buffer
-    int decompress_res = decompress(&compressed_buffer, dest);
+    int decompress_res = decompress(compressed_buffer, dest);
 
     switch (decompress_res) {
-        case -1:
-        case 1:
-            return qqmusic::result::mem_alloc_error;
-        case 2:
-            return qqmusic::result::data_destroy;
-        case 0:
-            break;
-        default:
-            return qqmusic::result::unknown_error;
+    case -1:
+    case 1:
+        return qqmusic::result({"memory alloc error"});
+    case 2:
+        return qqmusic::result({"data destroy when decompressing decoding lyric"});
+    case 0:
+        break;
+    default:
+        return qqmusic::result({"unknown error when decompressing decoded lyric"});
     }
 
-    return qqmusic::result::qrc_decode_ok;
+    return qqmusic::result({});
 }
 
-static int
-decompress(qqmusic::utils::buffer* src,
-           qqmusic::utils::buffer* dest)
-{
+static int decompress(const qqmusic::utils::buffer& src, qqmusic::utils::buffer& dest) {
     // prepare receive buffer
-    size_t   tmp_dest_size = src->get_size() * 4;
-    uint8_t* tmp_dest_head = (uint8_t*)malloc(tmp_dest_size);
+    size_t tmp_dest_size = src.size() * 4;
+    uint8_t* tmp_dest_head = (uint8_t*) malloc(tmp_dest_size);
 
     // prepare input buffer
-    size_t   src_size = src->get_size();
-    uint8_t* src_head = src->get_head();
+    size_t src_size = src.size();
+    const uint8_t* src_head = src.data();
 
     // ZEXTERN int ZEXPORT uncompress(Bytef *dest,   uLongf *destLen,
     //                                const Bytef *source, uLong sourceLen);
@@ -124,7 +105,7 @@ decompress(qqmusic::utils::buffer* src,
     }
 
     // write buffer into dest
-    dest->append(tmp_dest_head, tmp_dest_size);
+    dest.append(tmp_dest_head, tmp_dest_size);
 
     free(tmp_dest_head);
     return 0;
@@ -134,37 +115,35 @@ decompress(qqmusic::utils::buffer* src,
 // def qmc1_decrypt(data: bytearray) -> None:
 //     for i, _value in enumerate(data):
 //         data[i] ^= PRIVKEY[(i % 0x7FFF) & 0x7F] if i > 0x7FFF else PRIVKEY[i & 0x7F]
-static void
-qmc1_decrypt(qqmusic::utils::buffer* src)
-{
+static void qmc1_decrypt(qqmusic::utils::buffer& src) {
     uint8_t private_key[128] = {
-	    0xc3, 0x4a, 0xd6, 0xca, 0x90, 0x67, 0xf7, 0x52,
-	    0xd8, 0xa1, 0x66, 0x62, 0x9f, 0x5b, 0x09, 0x00,
+        0xc3, 0x4a, 0xd6, 0xca, 0x90, 0x67, 0xf7, 0x52,
+        0xd8, 0xa1, 0x66, 0x62, 0x9f, 0x5b, 0x09, 0x00,
 
-	    0xc3, 0x5e, 0x95, 0x23, 0x9f, 0x13, 0x11, 0x7e,
-	    0xd8, 0x92, 0x3f, 0xbc, 0x90, 0xbb, 0x74, 0x0e,
+        0xc3, 0x5e, 0x95, 0x23, 0x9f, 0x13, 0x11, 0x7e,
+        0xd8, 0x92, 0x3f, 0xbc, 0x90, 0xbb, 0x74, 0x0e,
 
-	    0xc3, 0x47, 0x74, 0x3d, 0x90, 0xaa, 0x3f, 0x51,
-	    0xd8, 0xf4, 0x11, 0x84, 0x9f, 0xde, 0x95, 0x1d,
+        0xc3, 0x47, 0x74, 0x3d, 0x90, 0xaa, 0x3f, 0x51,
+        0xd8, 0xf4, 0x11, 0x84, 0x9f, 0xde, 0x95, 0x1d,
 
-	    0xc3, 0xc6, 0x09, 0xd5, 0x9f, 0xfa, 0x66, 0xf9,
-	    0xd8, 0xf0, 0xf7, 0xa0, 0x90, 0xa1, 0xd6, 0xf3,
+        0xc3, 0xc6, 0x09, 0xd5, 0x9f, 0xfa, 0x66, 0xf9,
+        0xd8, 0xf0, 0xf7, 0xa0, 0x90, 0xa1, 0xd6, 0xf3,
 
-	    0xc3, 0xf3, 0xd6, 0xa1, 0x90, 0xa0, 0xf7, 0xf0,
-	    0xd8, 0xf9, 0x66, 0xfa, 0x9f, 0xd5, 0x09, 0xc6,
+        0xc3, 0xf3, 0xd6, 0xa1, 0x90, 0xa0, 0xf7, 0xf0,
+        0xd8, 0xf9, 0x66, 0xfa, 0x9f, 0xd5, 0x09, 0xc6,
 
-	    0xc3, 0x1d, 0x95, 0xde, 0x9f, 0x84, 0x11, 0xf4,
-	    0xd8, 0x51, 0x3f, 0xaa, 0x90, 0x3d, 0x74, 0x47,
+        0xc3, 0x1d, 0x95, 0xde, 0x9f, 0x84, 0x11, 0xf4,
+        0xd8, 0x51, 0x3f, 0xaa, 0x90, 0x3d, 0x74, 0x47,
 
-	    0xc3, 0x0e, 0x74, 0xbb, 0x90, 0xbc, 0x3f, 0x92,
-	    0xd8, 0x7e, 0x11, 0x13, 0x9f, 0x23, 0x95, 0x5e,
+        0xc3, 0x0e, 0x74, 0xbb, 0x90, 0xbc, 0x3f, 0x92,
+        0xd8, 0x7e, 0x11, 0x13, 0x9f, 0x23, 0x95, 0x5e,
 
-	    0xc3, 0x00, 0x09, 0x5b, 0x9f, 0x62, 0x66, 0xa1,
-	    0xd8, 0x52, 0xf7, 0x67, 0x90, 0xca, 0xd6, 0x4a,
+        0xc3, 0x00, 0x09, 0x5b, 0x9f, 0x62, 0x66, 0xa1,
+        0xd8, 0x52, 0xf7, 0x67, 0x90, 0xca, 0xd6, 0x4a,
     };
 
-    size_t size = src->get_size();
-    uint8_t* head = src->get_head();
+    size_t size = src.size();
+    uint8_t* head = src.data();
 
     for (size_t i = 0; i < size; ++i) {
         head[i] ^= i > 0x7fff ? private_key[(i % 0x7fff) & 0x7f] : private_key[i & 0x7f];
